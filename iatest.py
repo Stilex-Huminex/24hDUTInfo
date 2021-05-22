2d92cbdcee645bb4754046649fec136d087effbd
LoicGestin <loic.gestin34@gmail.com>
2021-05-22 12:45:39 +0200

    Update iatest.py

--- iatest.py
+++ iatest.py
@@ -1,26 +1,32 @@
 from interactions.game_interaction import GameInteraction
+from interactions.astar_algorithm import MapManager
 from random import *
 
 with GameInteraction("localhost", 2121) as runner:
+    runner("GETMAP")
     while True:
         
-       
-        runner("GETMAP")
-        runner.map[2][4]
 
         biker = (runner("GETBIKERS|0"))
-        biker[1][0]
         bx = int(biker[1][0][2])
         by = int(biker[1][0][4])
 
-        runner.map[bx][by]
-        if bx != 30 :
-            if runner.map[bx+1][by] == "R" :
-                print(runner("MOVE|0|R"))
-        if bx != 0 :
-            if runner.map[bx-1][by] == "R" :
-                print(runner("MOVE|0|L"))
         
-        print(runner("ENDTURN"))
+        mapMan = MapManager(runner.map)
+        result = mapMan.astar_search((bx,by), (30,30))
+        
+        
+       
+        i = 0
+        while i != len(result) :
+            if result[i][1] == (by-1) :
+                print(runner("MOVE|0|B"))
+            if result[i][0] == (bx+1) : 
+                print(runner("MOVE|0|R"))
+            if result[i][0] == (bx-1) :
+                print(runner("MOVE|0|L"))
+            i = i +1 
+            
+            
         
        
