bfff47524bbf101ff07b3c132c2b32c8699b1039
1Samper1 <quentin.arzalier@free.fr>
2021-05-22 10:48:28 +0200

    PlateauToMatrice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import socket
+import numpy as np
 
 
 class GameInteraction:
@@ -26,6 +27,22 @@ class GameInteraction:
 
     def prettify_command(self, command: bytes):
         return [self.parsename(command), self.parseparams(command)]
+
+    @staticmethod
+    def plateauToMatrice(plateau: str):
+        plateau = ','.join(plateau[i:i+31] for i in range(0, len(plateau), 31))
+        tab = plateau.split(',')
+        arr = []
+        for string in tab:
+            print(len(string))
+            arrNp = np.frombuffer(string.encode(), dtype='S1', count=-1)
+            arr2 = []
+            
+            for s in arrNp:
+                arr2.append(s.decode())
+            arr.append(arr2)
+        return arr
+
 
     def __init__(self, host, port):
         self.host = host
