36d2f2bc1106f13af3184d87bd971c3ca6b30f70
1Samper1 <quentin.arzalier@free.fr>
2021-05-22 08:06:44 +0200

    envoie msg

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import socket
  
 hote = "localhost"
-port = 12800
+port = 2121
  
 connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connexion_avec_serveur.connect((hote, port))
@@ -10,13 +10,13 @@ print("Connexion établie avec le serveur sur le port {}".format(port))
 
 msg_a_envoyer = b""
 while msg_a_envoyer != b"fin":
-    ##msg_a_envoyer = input("> ")
+    msg_a_envoyer = input(">> ")
     # Peut planter si vous tapez des caractères spéciaux
-    ##msg_a_envoyer = msg_a_envoyer.encode()
+    msg_a_envoyer = msg_a_envoyer.encode()
     # On envoie le message
-    ##connexion_avec_serveur.send(msg_a_envoyer)
+    connexion_avec_serveur.send(msg_a_envoyer)
     msg_recu = connexion_avec_serveur.recv(1024)
-    print(msg_recu.decode()) # Là encore, peut planter s'il y a des accents
+    print("<<" + msg_recu.decode()) # Là encore, peut planter s'il y a des accents
  
 print("Fermeture de la connexion")
 connexion_avec_serveur.close()
