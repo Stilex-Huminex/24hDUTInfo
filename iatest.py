8a961b15ffda24263f9ff43602344ac2098682dd
LoicGestin <loic.gestin34@gmail.com>
2021-05-22 10:56:38 +0200

    Update iatest.py

--- iatest.py
+++ iatest.py
@@ -1,5 +1,13 @@
-
+from interactions.game_interaction import GameInteraction
+from random import *
 
 with GameInteraction("localhost", 2121) as runner:
     while True:
+        
+        d = 8
+        while d != 0 :
+            n = randint(1,4)
+            if n == 1 :
+                if print(runner('MOVE|0|R'))[0] == 'OK' :
+                    d -= 1
         print(runner('ENDTURN'))
