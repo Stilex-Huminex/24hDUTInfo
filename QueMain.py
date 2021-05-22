c971aaa34faaa124606ac9e01b60156bb703005c
1Samper1 <quentin.arzalier@free.fr>
2021-05-22 12:55:36 +0200

    On gagne des points

--- QueMain.py
+++ QueMain.py
@@ -21,7 +21,6 @@ with GameInteraction("localhost", 2121) as runner:
     tab = commande[1][0].split(';')
     print(tab)
     code = tab[0]
-    print(runner.map[int(tab[2])][int(tab[3])])
 
     array = []
     if (runner.map[int(tab[2])-1][int(tab[3])] == 'R'):
@@ -37,6 +36,7 @@ with GameInteraction("localhost", 2121) as runner:
     print(route)
 
     old = (bx, by)
+    where = (0, 0)
     while len(route) != 0:
         where = route.pop(0)
         if (where[0] - old[0] == -1):
@@ -49,4 +49,43 @@ with GameInteraction("localhost", 2121) as runner:
             runner('MOVE|0|R')
         old = where
         print(where)
-    print(runner("TAKE|0|" + code))
+    
+    retour = "yes"
+    while retour != "NOK":
+        state = runner("TAKE|0|" + code)
+        retour = state[0]
+
+    mapMan = MapManager(runner.map)
+
+    array = []
+    if (runner.map[int(tab[4])-1][int(tab[5])] == 'R'):
+        array.append(mapMan.astar_search(where, (int(tab[4])-1, int(tab[5]))))
+    if (runner.map[int(tab[4])+1][int(tab[5])] == 'R'):
+        array.append(mapMan.astar_search(where, (int(tab[4])+1, int(tab[5]))))
+    if (runner.map[int(tab[4])][int(tab[5])-1] == 'R'):
+        array.append(mapMan.astar_search(where, (int(tab[4]), int(tab[5])-1)))
+    if (runner.map[int(tab[4])][int(tab[5])+1] == 'R'):
+        array.append(mapMan.astar_search(where, (int(tab[4]), int(tab[5])+1)))
+
+    route = min(array)
+    print(route)
+
+    old = where 
+    where = (0, 0)
+    while len(route) != 0:
+        where = route.pop(0)
+        if (where[0] - old[0] == -1):
+            runner('MOVE|0|T')
+        if (where[0] - old[0] == 1):
+            runner('MOVE|0|B')
+        if (where[1] - old[1] == -1):
+            runner('MOVE|0|L')
+        if (where[1] - old[1] == 1):
+            runner('MOVE|0|R')
+        old = where
+        print(where)
+
+    retour = "yes"
+    while retour != "NOK":
+        state = runner("DELIVER|0|" + code)
+        retour = state[0]
